b4233641130eccc54e8fb518cb10f13dbfe6e94b
Marius Drąsutis <marius.dras@gmail.com>
2019-09-15 11:36:16 +0300

    Added calculator component

---
+++ src/views/Calculator.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h5>Calculator</h5>
+
+        <div class="row">
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label for="resistance_input">Resistance</label>
+                    <input type="text" class="form-control form-control-sm" />
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label for="resistance_input">Wire length</label>
+                    <input type="text" class="form-control form-control-sm" />
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label for="resistance_input">Wire diameter</label>
+                    <input type="text" class="form-control form-control-sm" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
