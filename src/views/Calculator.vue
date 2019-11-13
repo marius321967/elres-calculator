c8dfa35c92abbff3b4c7263e1202972b10c83801
Marius Drąsutis <marius.dras@gmail.com>
2019-11-13 21:30:20 +0200

    Added calculator tabs to Calculator component

--- src/views/Calculator.vue
+++ src/views/Calculator.vue
@@ -2,6 +2,33 @@
     <div>
         <h5>Calculator</h5>
 
+        <div>
+            <ul class="nav nav-tabs">
+                <li class="nav-item">
+                    <router-link class="nav-link" to="temperature-coefficient">
+                        <strong>&alpha;</strong>
+                        Temperature coefficient calculator
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <strong>&rho;</strong>
+                        Resistivity calculator
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <strong>&Tau;</strong>
+                        Temperature calculator
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <router-view></router-view>
+        </div>
+
         <div class="row">
             <div class="col-md-4">
                 <div class="form-group">
