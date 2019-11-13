221dad3692da36fea84e70bb107c22064655981c
Marius Drąsutis <marius.dras@gmail.com>
2019-11-13 21:44:38 +0200

    Cleanup Calculator.vue

--- src/views/Calculator.vue
+++ src/views/Calculator.vue
@@ -2,7 +2,7 @@
     <div>
         <h5>Calculator</h5>
 
-        <div>
+        <div class="calculator-tabs">
             <ul class="nav nav-tabs">
                 <li class="nav-item">
                     <router-link class="nav-link" to="temperature-coefficient">
@@ -28,26 +28,11 @@
         <div>
             <router-view></router-view>
         </div>
-
-        <div class="row">
-            <div class="col-md-4">
-                <div class="form-group">
-                    <label for="resistance_input">Resistance</label>
-                    <input type="text" class="form-control form-control-sm" />
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="form-group">
-                    <label for="resistance_input">Wire length</label>
-                    <input type="text" class="form-control form-control-sm" />
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="form-group">
-                    <label for="resistance_input">Wire diameter</label>
-                    <input type="text" class="form-control form-control-sm" />
-                </div>
-            </div>
-        </div>
     </div>
 </template>
+
+<style>
+.calculator-tabs {
+    margin-bottom: 1rem;
+}
+</style>
