43b7a7fa5b2d89184f5511c968db6d6675c7a5be
Marius Drąsutis <marius.dras@gmail.com>
2019-11-13 21:31:01 +0200

    First calculator tab

--- src/router.js
+++ src/router.js
@@ -1,16 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import TemperatureCoefficientCalc from './views/TemperatureCoefficientCalc'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      component: Home
-    }
-  ]
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            component: Home,
+            children: [
+                { path: 'temperature-coefficient', component: TemperatureCoefficientCalc }
+            ]
+        }
+    ]
 })
