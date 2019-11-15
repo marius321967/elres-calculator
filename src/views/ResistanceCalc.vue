e4d740a2cffcafe33d451b1dbdd3107e76f129b9
Marius Drąsutis <marius.dras@gmail.com>
2019-11-15 17:39:13 +0200

    Resistance calculator with unfinished sub-components

--- src/views/ResistanceCalc.vue
+++ src/views/ResistanceCalc.vue
@@ -1,9 +1,22 @@
 <template>
-    <div>resistance</div>
+    <div>
+        <p>
+            Electrical resistance is used to express the resistance of a conductor to electrical current.
+            In material engineering, resistance is usually calculated from the resistivity of the conducting material
+            and the known dimensional properties of the conductor.
+        </p>
+
+        <from-basic></from-basic>
+        <hr />
+        <from-resistivity></from-resistivity>
+    </div>
 </template>
 
 <script>
-export default {
+import FromBasic from './Resistance/FromBasic'
+import FromResistivity from './Resistance/FromResistivity'
 
+export default {
+    components: { FromBasic, FromResistivity }
 }
 </script>
