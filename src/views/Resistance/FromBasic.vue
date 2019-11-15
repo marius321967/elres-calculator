7024a856bbec2ecfe55273bccb0a8da48ccbcee3
Marius Drąsutis <marius.dras@gmail.com>
2019-11-15 17:39:26 +0200

    Basic resistance calculator section

---
+++ src/views/Resistance/FromBasic.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <h5>From current and voltage</h5>
+
+        <div class="row">
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label for="voltage_input">Volage, V</label>
+                    <input type="text" class="form-control form-control-sm" id="voltage_input" v-model="voltage" />
+                </div> 
+            </div>
+            
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label for="current_input">Current, A</label>
+                    <input type="text" class="form-control form-control-sm" id="current_input" v-model="current" />
+                </div> 
+            </div>
+            
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label for="">Result: resistance R, &ohm;</label>
+                    <input type="text" class="form-control form-control-sm" :value="resistance" disabled>
+                </div> 
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        voltage: '', current: ''
+    }),
+
+    computed: {
+        resistance() {
+            if (this.voltage && this.current) return this.voltage / this.current
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
